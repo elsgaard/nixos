4bd3fc2a9a2c1958854e8749b0ca035e0290a054
Thomas Elsgaard <80691187+elsgaard@users.noreply.github.com>
2026-02-15 23:32:39 +0100

    Create configuration.nix

---
+++ hosts/configuration.nix
@@ -0,0 +1,34 @@
+{ modulesPath, config, lib, pkgs, ... }: {
+imports = [
+(modulesPath + "/installer/scan/not-detected.nix")
+(modulesPath + "/profiles/qemu-guest.nix")
+./disk-config.nix
+];
+
+boot.loader.grub = {
+efiSupport = true;
+efiInstallAsRemovable = true;
+};
+
+services.openssh.enable = true;
+
+   nix.settings = {
+     experimental-features = "nix-command flakes";
+   };
+   
+time.timeZone = "Europe/Copenhagen";
+i18n.defaultLocale = "en_US.UTF-8";
+console.keyMap = "dk";
+
+environment.systemPackages = [
+pkgs.curl
+pkgs.gitMinimal
+pkgs.jdk25
+];
+
+users.users.root.openssh.authorizedKeys.keys = [
+"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKeqZmlYFxsg1BqomySG+hRdJLcM3Q0YqGf13okIvzO4 eddsa-key-20260207"
+];
+
+system.stateVersion = "25.11";
+}
