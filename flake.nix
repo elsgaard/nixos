d24bff0f94425ec40254361f0010eb2116dff115
Thomas Elsgaard <80691187+elsgaard@users.noreply.github.com>
2026-02-15 22:10:26 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,20 @@
 {
-inputs = {
-nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-disko.url = "github:nix-community/disko";
-disko.inputs.nixpkgs.follows = "nixpkgs";
-};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-outputs = { nixpkgs, disko, ... }: {
-nixosConfigurations.wolf = nixpkgs.lib.nixosSystem {
-system = "x86_64-linux";
-modules = [
-disko.nixosModules.disko
-./configuration.nix
-./disk-config.nix
-];
-};
-};
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, disko, ... }:
+    {
+      nixosConfigurations.wolf = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./configuration.nix
+          ./disk-config.nix
+        ];
+      };
+    };
 }
