307217d245819ece705a519e94a070a7def44895
Thomas Elsgaard <80691187+elsgaard@users.noreply.github.com>
2026-02-15 21:56:20 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,63 +1,33 @@
-{ pkgs, ... }:
- 
- {
+{ modulesPath, config, lib, pkgs, ... }: {
+imports = [
+(modulesPath + "/installer/scan/not-detected.nix")
+(modulesPath + "/profiles/qemu-guest.nix")
+./disk-config.nix
+];
+
+boot.loader.grub = {
+efiSupport = true;
+efiInstallAsRemovable = true;
+};
+
+services.openssh.enable = true;
+
    nix.settings = {
      experimental-features = "nix-command flakes";
    };
    
-   environment.systemPackages = [
-     pkgs.vim
-     pkgs.git
-   ];
-   
-   fileSystems."/" = {
-     device = "/dev/disk/by-label/nixos";
-     fsType = "ext4";
-   };
-   fileSystems."/boot" = {
-     device = "/dev/disk/by-label/boot";
-     fsType = "ext4";
-   };
-   swapDevices = [
-     {
-       device = "/dev/disk/by-label/swap";
-     }
-   ];
-   
-   time.timeZone = "Europe/Copenhagen";
-   i18n.defaultLocale = "en_US.UTF-8";
-   console.keyMap = "dk";
-   
-   boot.loader.grub.enable = true;
-   boot.loader.grub.device = "/dev/sda";
-   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "ext4" ];
-   
-   users.users = {
-     root = {
-       hashedPassword = "!"; # disables password login for root
-     };
+time.timeZone = "Europe/Copenhagen";
+i18n.defaultLocale = "en_US.UTF-8";
+console.keyMap = "dk";
 
-     admin = {
-       isNormalUser = true;
-       extraGroups = [ "wheel" ];
-       openssh.authorizedKeys.keys = [
-         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKeqZmlYFxsg1BqomySG+hRdJLcM3Q0YqGf13okIvzO4 eddsa-key-20260207"
-       ];
-     };
-   };
+environment.systemPackages = [
+pkgs.curl
+pkgs.gitMinimal
+];
 
-   security.sudo.wheelNeedsPassword = false;
-   
-   services.openssh = {
-     enable = true;
-     settings = {
-       PermitRootLogin = "no";
-       PasswordAuthentication = false;
-       KbdInteractiveAuthentication = false;
-     };
-   };
-   
-   networking.firewall.allowedTCPPorts = [ 22 ];
-   
-   system.stateVersion = "25.11";
- }
+users.users.root.openssh.authorizedKeys.keys = [
+"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKeqZmlYFxsg1BqomySG+hRdJLcM3Q0YqGf13okIvzO4 eddsa-key-20260207"
+];
+
+system.stateVersion = "25.11";
+}
